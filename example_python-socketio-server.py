0616d730481ff4317fb64d87888c66943755d211
Brock Brown <brockrbrown@gmail.com>
2023-03-24 12:44:56 -0400

    Getting ready to connect the socket.io server to main.html

--- example_python-socketio-server.py
+++ example_python-socketio-server.py
@@ -1,5 +1,10 @@
 import socketio, eventlet, time, webbrowser
 
+# make sure there's only one instance of this server running
+# if there's already a server running, then open the client in a new tab
+# use singleton to do this
+
+
 sio = socketio.Server(cors_allowed_origins='*', max_http_buffer_size=1e+8)
 app = socketio.WSGIApp(sio)
 
@@ -22,5 +27,5 @@ def message(sid, data):
     sio.emit('message', "echo: " + data)
 
 if __name__ == '__main__':
-    webbrowser.open('example_socketio.html')
+    webbrowser.open('main.html')
     eventlet.wsgi.server(eventlet.listen(('localhost', 8000)), app)
